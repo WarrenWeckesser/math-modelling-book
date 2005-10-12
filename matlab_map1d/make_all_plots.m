f91c26d503ab0af53a5ac0cdc7ac56b083cc69c5
wweckesser <wweckesser@d78809a7-3d02-0410-b379-a10841dac46b>
2005-10-12 23:35:25 +0000

    Added make_all_plots script in matlab_map1d, really this time
    git-svn-id: svn+ssh://www.warrenweckesser.net/srv/svn/IMM@56 d78809a7-3d02-0410-b379-a10841dac46b

---
+++ matlab_map1d/make_all_plots.m
@@ -0,0 +1,20 @@
+%
+% Run the assorted scripts to create all the 1D map plots
+%
+
+close all
+popdataplot
+
+close all
+cobweb_script(1)
+
+close all
+linearcases(1)
+
+close all
+linearcases(2)
+
+close all
+noconclusion
+
+close all
