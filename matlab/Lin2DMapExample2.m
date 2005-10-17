28283d5ef2918ba37aa14a4eb62015dbbfa78410
wweckesser <wweckesser@d78809a7-3d02-0410-b379-a10841dac46b>
2005-10-17 22:22:21 +0000

    Added Lin2D*.m files in the matlab subdirectory
    git-svn-id: svn+ssh://www.warrenweckesser.net/srv/svn/IMM@71 d78809a7-3d02-0410-b379-a10841dac46b

---
+++ matlab/Lin2DMapExample2.m
@@ -0,0 +1,137 @@
+
+A = [1 -0.5; -0.75 -.25];
+
+xmin = -1;
+xmax = 1;
+nx = 21;
+ymin = -1;
+ymax = 1;
+ny = 21;
+
+lw = 2;
+
+clf;
+
+axis equal
+axis([xmin xmax ymin ymax])
+set(gca,'YTick',[-1 -0.5 0 0.5 1])
+set(gca,'fontsize',12)
+
+hold on
+%AddNullclines(A)
+%AddEigenSolutions0(A)
+AddAxes0(A)
+
+axis off
+text(1,-0.06,'1','fontsize',12);
+text(-1,-0.06,'-1','fontsize',12);
+text(0.03,1-0.02,'1','fontsize',12);
+text(0.03,-1,'-1','fontsize',12);
+
+%maxstep = 0.08;
+%opts = odeset('MaxStep',maxstep);
+%Tf = 20;
+%Tb = -20;
+
+N = 8;
+
+markerlw = 1;
+marker = 'k*';
+marker2 = 'k.';
+alen = 9;
+ata  = 8;
+
+ic = [.5; 1];
+x1 = ic;
+for k = 1:N
+    x2 = A*x1;
+    plot([x1(1) x2(1)],[x1(2) x2(2)],marker,'linewidth',markerlw);
+    plot([x1(1) x2(1)],[x1(2) x2(2)],marker2);
+    arrow(x1,x2,'tipangle',ata,'length',alen);
+    x1 = x2;
+end
+
+ic = [1/3; 1];
+x1 = ic;
+for k = 1:N
+    x2 = A*x1;
+    plot([x1(1) x2(1)],[x1(2) x2(2)],marker,'linewidth',markerlw);
+    plot([x1(1) x2(1)],[x1(2) x2(2)],marker2);
+    arrow(x1,x2,'tipangle',ata,'length',alen);
+    x1 = x2;
+end
+
+% ic = -ic;
+% x1 = ic;
+% for k = 1:N
+%     x2 = A*x1;
+%     plot([x1(1) x2(1)],[x1(2) x2(2)],marker,'linewidth',markerlw);
+%     plot([x1(1) x2(1)],[x1(2) x2(2)],marker2);
+%     arrow(x1,x2,'tipangle',ata,'length',alen);
+%     x1 = x2;
+% end
+
+% ic = -[1; -0.9];
+% x1 = ic;
+% for k = 1:N
+%     x2 = A*x1;
+%     plot([x1(1) x2(1)],[x1(2) x2(2)],marker,'linewidth',markerlw);
+%     plot([x1(1) x2(1)],[x1(2) x2(2)],marker2);
+%     arrow(x1,x2,'tipangle',ata,'length',alen);
+%     x1 = x2;
+% end
+
+ic = [0.1; 1];
+x1 = ic;
+for k = 1:N
+    x2 = A*x1;
+    plot([x1(1) x2(1)],[x1(2) x2(2)],marker,'linewidth',markerlw);
+    plot([x1(1) x2(1)],[x1(2) x2(2)],marker2);
+    arrow(x1,x2,'tipangle',ata,'length',alen);
+    x1 = x2;
+end
+
+
+% N = 5;
+% 
+% 
+% ic = [1; -1];
+% x1 = ic;
+% for k = 1:N
+%     x2 = A*x1;
+%     plot([x1(1) x2(1)],[x1(2) x2(2)],marker,'linewidth',markerlw);
+%     plot([x1(1) x2(1)],[x1(2) x2(2)],marker2);
+%     arrow(x1,x2,'tipangle',ata,'length',alen);
+%     x1 = x2;
+% end
+% 
+% ic = -[1; -1];
+% x1 = ic;
+% for k = 1:N
+%     x2 = A*x1;
+%     plot([x1(1) x2(1)],[x1(2) x2(2)],marker,'linewidth',markerlw);
+%     plot([x1(1) x2(1)],[x1(2) x2(2)],marker2);
+%     arrow(x1,x2,'tipangle',ata,'length',alen);
+%     x1 = x2;
+% end
+% 
+% ic = 0.08*[0.5; 1];
+% x1 = ic;
+% for k = 1:N
+%     x2 = A*x1;
+%     plot([x1(1) x2(1)],[x1(2) x2(2)],marker,'linewidth',markerlw);
+%     plot([x1(1) x2(1)],[x1(2) x2(2)],marker2);
+%     arrow(x1,x2,'tipangle',ata,'length',alen);
+%     x1 = x2;
+% end
+% 
+% ic = -0.08*[0.5; 1];
+% x1 = ic;
+% for k = 1:N
+%     x2 = A*x1;
+%     plot([x1(1) x2(1)],[x1(2) x2(2)],marker,'linewidth',markerlw);
+%     plot([x1(1) x2(1)],[x1(2) x2(2)],marker2);
+%     arrow(x1,x2,'tipangle',ata,'length',alen);
+%     x1 = x2;
+% end
+
---
+++ matlab/Lin2DMapExampleComplexrz2.m
@@ -0,0 +1,58 @@
+
+P = [1 1; 1 -2];
+a = 1/2;
+b = sqrt(3)/2;
+R = [a -b; b a];
+A = P*R*inv(P);
+
+xmin = -1;
+xmax = 1;
+nx = 21;
+ymin = -1;
+ymax = 1;
+ny = 21;
+
+lw = 2;
+
+clf;
+
+axis equal
+axis([xmin xmax ymin ymax])
+set(gca,'YTick',[-1 -0.5 0 0.5 1])
+set(gca,'fontsize',12)
+
+hold on
+%AddNullclines(A)
+%AddEigenSolutions0(A)
+AddAxes0(A)
+
+axis off
+text(1,-0.06,'1','fontsize',12);
+text(-1,-0.06,'-1','fontsize',12);
+text(0.03,1-0.02,'1','fontsize',12);
+text(0.03,-1,'-1','fontsize',12);
+
+%maxstep = 0.08;
+%opts = odeset('MaxStep',maxstep);
+%Tf = 20;
+%Tb = -20;
+
+N = 13;
+
+markerlw = 1;
+marker = 'k*';
+marker2 = 'k.';
+alen = 9;
+ata  = 8;
+
+ic = [0.5; 0.25];
+x1 = ic;
+for k = 1:N
+    x2 = A*x1;
+    plot([x1(1) x2(1)],[x1(2) x2(2)],marker,'linewidth',markerlw);
+    plot([x1(1) x2(1)],[x1(2) x2(2)],marker2);
+    arrow(x1,x2,'tipangle',ata,'length',alen);
+    x1 = x2;
+end
+
+text(0.55,0.3,'x_0=x_6','fontsize',12,'fontweight','bold')
