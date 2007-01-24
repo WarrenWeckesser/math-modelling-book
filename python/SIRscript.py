a9b196fdf51af652a502f5f952a0cd48c4609974
dschult <dschult@d78809a7-3d02-0410-b379-a10841dac46b>
2007-01-24 19:49:06 +0000

    Cleaned up using svn:ignore so svn status is now quite clean. Changed dome.sh to compile Changed python/SIRscript.py to work without Gnuplot.py module so long as gnuplot is available.
    git-svn-id: svn+ssh://www.warrenweckesser.net/srv/svn/IMM@206 d78809a7-3d02-0410-b379-a10841dac46b

--- python/SIRscript.py
+++ python/SIRscript.py
@@ -11,7 +11,7 @@
 #
 from scipy import *
 from scipy.integrate import odeint
-import Gnuplot
+#import Gnuplot
 #
 # Get the SIR vector field function from SIRvectorfield.py.
 #
@@ -38,24 +38,41 @@ t = arange(0.0,tfinal,tfinal/200)
 # Call the ODE solver.
 #
 x = odeint(SIRvectorfield,x0,t,args=(r,g))
-#
-# Plot the solution.
-#
-gp = Gnuplot.Gnuplot(persist=1)
-gp('set data style lines')
-title = 'Solution of the SIR Model  (r='+str(r)+', gamma='+str(g)+')'
-gp.title(title)
-gp.xlabel('t')
-gp('set key')
-Splot = Gnuplot.Data(t,x[:,0],title='S')
-Iplot = Gnuplot.Data(t,x[:,1],title='I')
-Rplot = Gnuplot.Data(t,x[:,2],title='R')
-gp.plot(Splot,Iplot,Rplot)
-#
-# The following replots the data to a file
-# using Encapsulated Postscript (eps) format.
-#
-gp('set terminal postscript eps color ')
-gp('set output "SIRscript_output.eps"')
-gp.replot()
 
+outdata=zeros((len(t),4))
+outdata[:,0]=t
+outdata[:,1:]=x
+outdata.tofile(open('SIRdata.txt','w'),sep=' ')
+
+fh=open('SIRdata.gpi','w')
+print >>fh,"""
+set data style lines
+set title 'Solution of the SIR Model  (r=%s, gamma=%s)
+set xlabel 't'
+set key
+set terminal postscript eps color
+set output "SIRscript_output.eps"
+plot "SIRdata.txt" u 1:2 title 'S',"SIRdata.txt" u 1:3 t 'I',"SIRdata.txt" u 1:4 t 'R'
+"""
+print "Now run gnuplot as:    gnuplot SIRdata.gpi"
+##
+## Plot the solution.
+##
+#gp = Gnuplot.Gnuplot(persist=1)
+#gp('set data style lines')
+#title = 'Solution of the SIR Model  (r='+str(r)+', gamma='+str(g)+')'
+#gp.title(title)
+#gp.xlabel('t')
+#gp('set key')
+#Splot = Gnuplot.Data(t,x[:,0],title='S')
+#Iplot = Gnuplot.Data(t,x[:,1],title='I')
+#Rplot = Gnuplot.Data(t,x[:,2],title='R')
+#gp.plot(Splot,Iplot,Rplot)
+##
+## The following replots the data to a file
+## using Encapsulated Postscript (eps) format.
+##
+#gp('set terminal postscript eps color ')
+#gp('set output "SIRscript_output.eps"')
+#gp.replot()
+#
