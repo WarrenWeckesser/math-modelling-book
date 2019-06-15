8d02f0f75c4d3c06d376e8efe8ff88a4d3d8c982
Warren Weckesser <warren.weckesser@gmail.com>
2019-06-15 00:01:29 -0400

    Update SIRscript.py

--- python/SIRscript.py
+++ python/SIRscript.py
@@ -1,26 +1,25 @@
 #
 # File:  SIRscript.py
 # Author: Warren Weckesser
-#         Department of Mathematics
-#         Colgate University
 #
-# Copyright(c) 2005, 2006 Warren Weckesser
+# Copyright(c) 2005, 2006, 2019 Warren Weckesser
 #
-#
-# Load the SciPy libraries and Gnupot libraries.
-#
+
 import numpy as np
 from scipy.integrate import odeint
-#import Gnuplot
+import matplotlib.pyplot as plt
+
 #
 # Get the SIR vector field function from SIRvectorfield.py.
 #
 from SIRvectorfield import SIRvectorfield
+
 #
 # Set the parameters.
 #
 r = 0.12
 g = 0.07
+
 #
 # Set the initial conditions and put into the array x0.
 #
@@ -28,12 +27,14 @@ S0 = 0.995
 I0 = 0.005
 R0 = 0.0
 x0 = [S0, I0, R0]
+
 #
 # Create the time samples for the output of the
 # ODE solver.
 #
 tfinal = 250.0
 t = np.arange(0.0, tfinal, tfinal/200)
+
 #
 # Call the ODE solver.
 #
@@ -44,38 +45,12 @@ outdata[:,0] = t
 outdata[:,1:] = x
 np.savetxt('SIRdata.txt', outdata)
 
-fh = open('SIRdata.gpi','w')
-print >> fh, """
-set style data lines
-set title 'Solution of the SIR Model  (r=%s, gamma=%s)
-set xlabel 't'
-set key
-set terminal postscript eps color
-set output "SIRscript_output.eps"
-plot "SIRdata.txt" u 1:2 title 'S',"SIRdata.txt" u 1:3 t 'I',"SIRdata.txt" u 1:4 t 'R'
-"""
-fh.close()
+plt.plot(t, x[:, 0], '-', label='S')
+plt.plot(t, x[:, 1], '--', label='I')
+plt.plot(t, x[:, 2], ':', label='R')
 
-print "Now run gnuplot as:    gnuplot SIRdata.gpi"
-
-##
-## Plot the solution.
-##
-#gp = Gnuplot.Gnuplot(persist=1)
-#gp('set data style lines')
-#title = 'Solution of the SIR Model  (r='+str(r)+', gamma='+str(g)+')'
-#gp.title(title)
-#gp.xlabel('t')
-#gp('set key')
-#Splot = Gnuplot.Data(t,x[:,0],title='S')
-#Iplot = Gnuplot.Data(t,x[:,1],title='I')
-#Rplot = Gnuplot.Data(t,x[:,2],title='R')
-#gp.plot(Splot,Iplot,Rplot)
-##
-## The following replots the data to a file
-## using Encapsulated Postscript (eps) format.
-##
-#gp('set terminal postscript eps color ')
-#gp('set output "SIRscript_output.eps"')
-#gp.replot()
-#
+plt.xlabel('t')
+plt.title('Solution of the SIR Model (r=%s, gamma=%s)' % (r, g))
+plt.legend(shadow=True)
+plt.grid()
+plt.savefig('SIRscript_output.eps')
